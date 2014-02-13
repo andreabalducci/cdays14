6b4bf7be2d8686a56df055ffa431809cf70f3740
Andrea Balducci <mtb.snowboard@gmail.com>
2014-02-13 15:04:27 +0100

    Added BookingRequest

---
+++ Bookings.Engine/Domain/Bookings/BookingRequest/BookingRequestAggregate.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookings.Engine.Support;
+
+namespace Bookings.Engine.Domain.Bookings.BookingRequest
+{
+    public class BookingRequestAggregate : Aggregate<BookingRequestState, BookingRequestId>
+    {
+        public BookingRequestAggregate(BookingRequestState state) : base(state)
+        {
+        }
+
+        public BookingRequestAggregate() : base(new BookingRequestState())
+        {
+        }
+
+        public void Create(BookingRequestId id)
+        {
+            RaiseEvent(new BookingRequestCreated(id));
+        }
+    }
+
+    public class BookingRequestCreated
+    {
+        public BookingRequestId BookingRequestId { get; set; }
+
+        public BookingRequestCreated(BookingRequestId id)
+        {
+            BookingRequestId = id;
+        }
+    }
+
+    public class BookingRequestState : AggregateState<BookingRequestId>
+    {
+        public void On(BookingRequestCreated e)
+        {
+            this.Id = e.BookingRequestId;
+        }
+    }
+
+    public class BookingRequestId : AggreagateId
+    {
+    }
+}
---
+++ Bookings.Specs/Domain/BookingRequestSpecs/when_is_created.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Bookings.Engine.Domain.Bookings.BookingRequest;
+using Bookings.Specs.Support;
+using Machine.Specifications;
+
+// ReSharper disable InconsistentNaming
+
+namespace Bookings.Specs.Domain.BookingRequestSpecs
+{
+    public class when_is_created : in_bookingRequest_context
+    {
+
+    }
+
+    [Subject("Given a new BookingRequest")]
+    public class in_bookingRequest_context : AbstractSpecification<BookingRequestAggregate,BookingRequestState, BookingRequestId>
+    {
+        private static BookingRequestId _id = new BookingRequestId();
+        
+        Establish context = () => 
+            SetUp();
+
+        Because of = () => 
+            Aggregate.Create(_id);
+
+        //
+        // State changes
+        //
+        It the_identifier_should_be_set = () =>
+            State.Id.ShouldBeLike(_id);
+
+
+        //
+        // Events
+        // 
+        It ResourceCreated_event_should_be_raised = () =>
+            LastRaisedEventOfType<BookingRequestCreated>().ShouldNotBeNull();
+
+        It ResourceCreated_event_shoud_have_the_resource_id_set = () =>
+            LastRaisedEventOfType<BookingRequestCreated>()
+                .BookingRequestId.ShouldBeLike(_id);
+
+        //
+        // Invariants
+        //
+        It invariants_are_satisfied = () =>
+            Aggregate.EnsureAllInvariants();
+    }
+}
